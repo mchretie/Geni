6f0d4e1b1dbf4edddd56635ed820b7ee476e1c31
Thomas <thomas.ocuilleanain@ulb.be>
2023-03-01 13:38:49 +0100

    Refactored

--- src/ulb/infof307/g01/Main.java
+++ src/ulb/infof307/g01/Main.java
@@ -1,11 +1,5 @@
 package ulb.infof307.g01;
 
-import ulb.infof307.g01.model.Card;
-import ulb.infof307.g01.model.Deck;
-
-import java.util.Collections;
-import java.util.Iterator;
-
 public class Main {
     public static void main(String[] args) {
     }
