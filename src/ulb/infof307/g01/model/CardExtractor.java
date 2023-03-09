7b41a97882a453551cfdd2e88e0303871cc7ab7f
Zoé Moeremans <zoemoeremans@Zoes-MacBook-Air.local>
2023-03-09 13:10:00 +0100

    interface replaced by inheritance + added random draw (tirage)

--- src/ulb/infof307/g01/model/CardExtractor.java
+++ src/ulb/infof307/g01/model/CardExtractor.java
@@ -1,8 +1,33 @@
 package ulb.infof307.g01.model;
 
-/**
- * interface implementing different types of card draw (tirage)
- */
-public interface CardExtractor {
-    public Card getNextCard();
+import java.util.ArrayList;
+import java.util.List;
+
+abstract public class CardExtractor {
+
+    final List<Card> sortedCards;
+    private Integer currentCardIndex; // to know where we are in the deck during the drawing
+
+    public CardExtractor(Deck deck) {
+        this.sortedCards = new ArrayList<>(deck.getCards());
+        this.currentCardIndex = -1;
+        this.sortDeck();
+    }
+
+    abstract void sortDeck();
+
+    public List<Card> getSortedCards() {
+        return sortedCards;
+    }
+
+    public Card getNextCard() {
+        this.currentCardIndex += 1;
+
+        if (this.currentCardIndex == this.sortedCards.size()) {
+            this.currentCardIndex = 0;
+            this.sortDeck();
+
+        }
+        return this.sortedCards.get(currentCardIndex);
+    }
 }
--- src/ulb/infof307/g01/model/CardExtractorByKnowledge.java
+++ src/ulb/infof307/g01/model/CardExtractorByKnowledge.java
@@ -4,34 +4,14 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class CardExtractorByKnowledge implements CardExtractor {
-
-    final private List<Card> sortedCards;
-    private Integer currentCardIndex; // to know where we are in the deck during the drawing
+public class CardExtractorByKnowledge extends CardExtractor {
 
     public CardExtractorByKnowledge(Deck deck) {
-        this.sortedCards = new ArrayList<>(deck.getCards());
-        this.currentCardIndex = -1;
-        this.sortDeck();
+        super(deck);
     }
-
-    private void sortDeck() {
+    @Override
+    void sortDeck() {
         Collections.sort(this.sortedCards);
     }
 
-    public List<Card> getSortedCards() {
-        return sortedCards;
-    }
-
-    @Override
-    public Card getNextCard() {
-        this.currentCardIndex += 1;
-
-        if (this.currentCardIndex == this.sortedCards.size()) {
-            this.currentCardIndex = 0;
-            this.sortDeck();
-
-        }
-        return this.sortedCards.get(currentCardIndex);
-    }
 }
