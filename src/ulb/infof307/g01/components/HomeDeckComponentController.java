34abee6c113f2fd95c434efde1111e062453ffb0
louis <louis.vanstappen@gmail.com>
2023-03-13 03:26:09 +0100

    Added options panel

--- src/ulb/infof307/g01/components/HomeDeckComponentController.java
+++ src/ulb/infof307/g01/components/HomeDeckComponentController.java
@@ -1,4 +1,41 @@
 package ulb.infof307.g01.components;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import org.kordamp.ikonli.javafx.FontIcon;
+
 public class HomeDeckComponentController {
+    @FXML
+    public Button toggleOptions;
+    @FXML
+    public FontIcon threeDotIcon;
+    public FontIcon threeDotIconShown;
+    public StackPane optionsOverlay;
+    public StackPane optionsPanel;
+    @FXML
+    private String deckName;
+
+    public void setDeckName(String deckName) {
+        // TODO fix this
+        this.deckName = deckName;
+    }
+
+
+    public void handleHoverOptions(MouseEvent mouseEvent) {
+        threeDotIcon.setIconColor(Color.web("#FFFFFF"));
+        threeDotIconShown.setIconColor(Color.web("#FFFFFF"));
+    }
+
+    public void handleHoverOptionsExit(MouseEvent mouseEvent) {
+        threeDotIcon.setIconColor(Color.web("#000000"));
+        threeDotIconShown.setIconColor(Color.web("#000000"));
+    }
+
+    public void handleToggleOptionClicked(MouseEvent mouseEvent) {
+        optionsOverlay.setVisible(!optionsOverlay.isVisible());
+        optionsPanel.setVisible(!optionsPanel.isVisible());
+    }
 }
--- src/ulb/infof307/g01/view/HomeViewController.java
+++ src/ulb/infof307/g01/view/HomeViewController.java
@@ -1,14 +1,29 @@
 package ulb.infof307.g01.view;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import org.kordamp.ikonli.javafx.FontIcon;
+import ulb.infof307.g01.components.HomeDeckComponentController;
 
-public class HomeViewController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HomeViewController implements Initializable {
     @FXML
-    public FontIcon searchIcon;
-    public FontIcon createDeckIcon;
+    Button createDeckButton;
+    @FXML
+    FontIcon searchIcon;
+    @FXML
+    FontIcon createDeckIcon;
+    @FXML
+    HomeDeckComponentController deck1Controller;
+    @FXML
+    HomeDeckComponentController deck2Controller;
+    @FXML
+    HomeDeckComponentController deck3Controller;
 
     @FXML
     public void handleSearchHover() {
@@ -26,5 +41,12 @@ public class HomeViewController {
 
     public void handleCreateDeckExit(MouseEvent mouseEvent) {
         createDeckIcon.setIconColor(Color.web("#000000"));
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        deck1Controller.setDeckName("Deck 1");
+        deck2Controller.setDeckName("Deck 2");
+        deck3Controller.setDeckName("Deck 3");
     }
 }
