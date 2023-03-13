152f261db7459efd4af6adb09c8cbc428000aaac
louis <louis.vanstappen@gmail.com>
2023-03-13 01:32:44 +0100

    Implemented main view switcher

--- src/ulb/infof307/g01/view/MainViewController.java
+++ src/ulb/infof307/g01/view/MainViewController.java
@@ -4,39 +4,59 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class MainViewController implements Initializable {
     @FXML
-    public Label topBar;
-
-    @FXML
     public Button goBack;
+    public Label topBar;
+    public Button home;
+    public Button deck;
+    public Button about;
+    public BorderPane homeView;
+    public BorderPane editDeckView;
+
+    private List<BorderPane> views;
 
     private MainViewListener listener;
 
-    public void setListener(MainViewListener listener){
+    public void setListener(MainViewListener listener) {
         System.out.println("setListener");
         this.listener = listener;
     }
 
+    private void hideViewsExcept(BorderPane view) {
+        for (BorderPane v : views) {
+            v.setVisible(v == view);
+        }
+    }
+
     public void setMainView() {
-        topBar.setText("Main Deck");
+        topBar.setText("Main View");
+        goBack.setVisible(false);
+        hideViewsExcept(homeView);
     }
 
     public void setEditDeckView() {
         topBar.setText("Edit Deck");
+        goBack.setVisible(true);
+        hideViewsExcept(editDeckView);
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        views = new ArrayList<>(Arrays.asList(homeView, editDeckView));
         setMainView();
     }
 
 
-    public interface MainViewListener{
+    public interface MainViewListener {
         void onAddCardButton();
     }
 }
