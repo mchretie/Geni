20c8a2979e2ffa8a477a121c6b7345d136a287e5
louis <louis.vanstappen@gmail.com>
2023-03-13 02:30:36 +0100

    Added dynamic deck component

---
+++ src/ulb/infof307/g01/components/HomeDeckComponentController.java
@@ -0,0 +1,4 @@
+package ulb.infof307.g01.components;
+
+public class HomeDeckComponentController {
+}
--- src/ulb/infof307/g01/view/HomeViewController.java
+++ src/ulb/infof307/g01/view/HomeViewController.java
@@ -1,4 +1,30 @@
 package ulb.infof307.g01.view;
 
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import org.kordamp.ikonli.javafx.FontIcon;
+
 public class HomeViewController {
+    @FXML
+    public FontIcon searchIcon;
+    public FontIcon createDeckIcon;
+
+    @FXML
+    public void handleSearchHover() {
+        searchIcon.setIconColor(Color.web("#FFFFFF"));
+    }
+
+    @FXML
+    public void handleSearchExit() {
+        searchIcon.setIconColor(Color.web("#000000"));
+    }
+
+    public void handleCreateDeckHover(MouseEvent mouseEvent) {
+        createDeckIcon.setIconColor(Color.web("#FFFFFF"));
+    }
+
+    public void handleCreateDeckExit(MouseEvent mouseEvent) {
+        createDeckIcon.setIconColor(Color.web("#000000"));
+    }
 }
