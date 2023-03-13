dc61334d8cc0eaf2100cf0b25a2f4372d694c0fe
louis <louis.vanstappen@gmail.com>
2023-03-13 13:47:08 +0100

    Stack pane options icon

--- src/ulb/infof307/g01/components/HomeDeckComponentController.java
+++ src/ulb/infof307/g01/components/HomeDeckComponentController.java
@@ -5,6 +5,7 @@ import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import org.kordamp.ikonli.javafx.FontIcon;
 
 public class HomeDeckComponentController {
@@ -12,11 +13,20 @@ public class HomeDeckComponentController {
     public Button toggleOptions;
     @FXML
     public FontIcon threeDotIcon;
-    public FontIcon threeDotIconShown;
+    @FXML
     public StackPane optionsOverlay;
+    @FXML
     public StackPane optionsPanel;
     @FXML
-    private String deckName;
+    public FontIcon editDeckIcon;
+    @FXML
+    public FontIcon removeDeckIcon;
+    @FXML
+    public FontIcon shareDeckIcon;
+    @FXML
+    public Rectangle optionsOverlayRect;
+    @FXML
+    private String deckName = "Deck Name";
 
     public void setDeckName(String deckName) {
         // TODO fix this
@@ -26,16 +36,47 @@ public class HomeDeckComponentController {
 
     public void handleHoverOptions(MouseEvent mouseEvent) {
         threeDotIcon.setIconColor(Color.web("#FFFFFF"));
-        threeDotIconShown.setIconColor(Color.web("#FFFFFF"));
     }
 
     public void handleHoverOptionsExit(MouseEvent mouseEvent) {
         threeDotIcon.setIconColor(Color.web("#000000"));
-        threeDotIconShown.setIconColor(Color.web("#000000"));
     }
 
     public void handleToggleOptionClicked(MouseEvent mouseEvent) {
-        optionsOverlay.setVisible(!optionsOverlay.isVisible());
+        optionsOverlayRect.setVisible(!optionsOverlayRect.isVisible());
         optionsPanel.setVisible(!optionsPanel.isVisible());
+    }
+
+    public void handleToggleEditDeckClicked(MouseEvent mouseEvent) {
+    }
+
+    public void handleHoverEditDeck(MouseEvent mouseEvent) {
+        editDeckIcon.setIconColor(Color.web("#FFFFFF"));
+    }
+
+    public void handleHoverEditDeckExit(MouseEvent mouseEvent) {
+        editDeckIcon.setIconColor(Color.web("#000000"));
+    }
+
+    public void handleToggleRemoveDeckClicked(MouseEvent mouseEvent) {
+    }
+
+    public void handleHoverRemoveDeck(MouseEvent mouseEvent) {
+        removeDeckIcon.setIconColor(Color.web("#FFFFFF"));
+    }
+
+    public void handleHoverRemoveDeckExit(MouseEvent mouseEvent) {
+        removeDeckIcon.setIconColor(Color.web("#000000"));
+    }
+
+    public void handleToggleShareDeckClicked(MouseEvent mouseEvent) {
+    }
+
+    public void handleHoverShareDeck(MouseEvent mouseEvent) {
+        shareDeckIcon.setIconColor(Color.web("#FFFFFF"));
+    }
+
+    public void handleHoverShareDeckExit(MouseEvent mouseEvent) {
+        shareDeckIcon.setIconColor(Color.web("#000000"));
     }
 }
