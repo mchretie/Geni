7f9fedba7c6b08c66d7700e598602cf8be95faeb
louis <louis.vanstappen@gmail.com>
2023-03-22 18:32:04 +0100

    Added deck api routes

--- src/ulb/infof307/g01/backend/deck/DeckHandler.java
+++ src/ulb/infof307/g01/backend/deck/DeckHandler.java
@@ -4,9 +4,13 @@ import spark.Request;
 import spark.Response;
 import ulb.infof307.g01.backend.Handler;
 
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 import java.util.logging.Logger;
 
 import static spark.Spark.*;
+import static ulb.infof307.g01.backend.utils.JsonUtil.json;
 
 public class DeckHandler implements Handler {
     private static final Logger logger = Logger.getLogger("DeckHandler");
@@ -15,12 +19,39 @@ public class DeckHandler implements Handler {
     public void init() {
         logger.info("Starting deck handler");
         path("/api", () -> {
-            get("/deck", this::getDeck);
+            path("/deck", () -> {
+                get(":user_id", this::getDeck, json());
+                post(":user_id", this::saveDeck, json());
+                delete(":deck_id", this::deleteDeck, json());
+                get("/all/:user_id", this::getAllDecks, json());
+                get("/search/:user_search", this::searchDecks, json());
+            });
         });
         logger.info("Deck handler started");
     }
 
     private String getDeck(Request req, Response res) {
+        UUID userId = UUID.fromString(req.params(":user_id"));
         return "You have reached the deck handler";
+    }
+
+    private Map<String, String> saveDeck(Request req, Response res) {
+        UUID userId = UUID.fromString(req.params(":user_id"));
+        return null;
+    }
+
+    private Map<String, String> deleteDeck(Request req, Response res) {
+        UUID userId = UUID.fromString(req.params(":user_id"));
+        return null;
+    }
+
+    private Map<String, String> getAllDecks(Request req, Response res) {
+        UUID userId = UUID.fromString(req.params(":user_id"));
+        return null;
+    }
+
+    private List<String> searchDecks(Request req, Response res){
+        String userSearch = req.params(":user_search");
+        return null;
     }
 }
---
+++ src/ulb/infof307/g01/backend/tag/TagHandler.java
@@ -0,0 +1,10 @@
+package ulb.infof307.g01.backend.tag;
+
+import ulb.infof307.g01.backend.Handler;
+
+public class TagHandler implements Handler {
+    @Override
+    public void init() {
+
+    }
+}
