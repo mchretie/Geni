7f831ad4787b9909d45940c5dd375e734adb4c14
louis <louis.vanstappen@gmail.com>
2023-03-22 13:18:30 +0100

    Added logs

--- src/ulb/infof307/g01/backend/Server.java
+++ src/ulb/infof307/g01/backend/Server.java
@@ -1,8 +1,14 @@
 package ulb.infof307.g01.backend;
+import java.util.logging.Logger;
+
 import static spark.Spark.*;
 
 public class Server {
     public static void main(String[] args) {
+        Logger logger = Logger.getLogger("Server");
+        logger.info("Starting server");
+        port(8080);
         get("/hello", (req, res) -> "Hello World");
+
     }
 }
