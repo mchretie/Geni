6184ef5de47e4afe3759e547ded376b938cb6646
louis <louis.vanstappen@gmail.com>
2023-03-23 16:11:57 +0100

    Fixed tag id not properly updated on name conflict

--- test/ulb/infof307/g01/database/TestDeckDAO.java
+++ test/ulb/infof307/g01/database/TestDeckDAO.java
@@ -151,6 +151,25 @@ public class TestDeckDAO extends DatabaseUsingTest {
     }
 
     @Test
+    void saveDeck_MultipleTagsAdded_DeckUpdated() throws SQLException {
+        Deck deck1 = new Deck("deck1");
+        Deck deck2 = new Deck("deck2");
+        Tag tag1 = new Tag("tag");
+        Tag tag2 = new Tag("tag");
+
+        deck1.addTag(tag1);
+        deckDAO.saveDeck(deck1);
+
+        deck2.addTag(tag2);
+        deckDAO.saveDeck(deck2);
+
+        System.out.println(deck1.getTags().get(0).getName());
+        System.out.println(deck2.getTags().get(0).getName());
+
+        assertEquals(deck1.getTags().get(0).getId(), deck2.getTags().get(0).getId());
+    }
+
+    @Test
     void getAllDecks_NoDecks_EmptyList() throws SQLException {
         assertTrue(deckDAO.getAllDecks().isEmpty());
     }
