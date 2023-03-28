8fcac0d583f6ef9a9157679019f094d510f02e35
louis <louis.vanstappen@gmail.com>
2023-03-28 20:00:23 +0200

    Implemented JWTokens

--- src/ulb/infof307/g01/server/handler/UserAccountHandler.java
+++ src/ulb/infof307/g01/server/handler/UserAccountHandler.java
@@ -3,6 +3,7 @@ package ulb.infof307.g01.server.handler;
 import spark.Request;
 import spark.Response;
 import ulb.infof307.g01.server.database.dao.UserDAO;
+import ulb.infof307.g01.server.service.JWTService;
 
 import java.util.Map;
 
@@ -12,6 +13,7 @@ import static spark.Spark.*;
 public class UserAccountHandler extends Handler {
 
     private final UserDAO userDAO = new UserDAO();
+    private final JWTService jwtService = new JWTService();
 
     @Override
     public void init() {
@@ -21,6 +23,7 @@ public class UserAccountHandler extends Handler {
             path("/user", () -> {
                 post("/register", this::registerUser);
                 get("/login", this::loginUser);
+                get("/test", this::testAuth);
 //                get("/logout", this::logoutUser);
 //                get("/delete", this::deleteUser);
 //                get("/update", this::updateUser);
@@ -30,10 +33,27 @@ public class UserAccountHandler extends Handler {
         logStarted();
     }
 
+    private Map<String, String> testAuth(Request request, Response response) {
+        String token = request.cookie("token");
+        boolean isTokenValid = jwtService.isTokenValid(token);
+        if (isTokenValid) {
+            String username = jwtService.getUsernameFromToken(token);
+            System.out.println(username);
+            return successfulResponse;
+        } else {
+            System.out.println("Token is not valid");
+            return failedResponse;
+        }
+    }
+
     private Map<String, String> loginUser(Request request, Response response) {
         String username = request.queryParams("username");
         String password = request.queryParams("password");
         boolean isValidLogin = userDAO.loginUser(username, password);
+        if (isValidLogin) {
+            String token = jwtService.generateToken(username);
+            response.cookie("token", token);
+        }
         return isValidLogin ? successfulResponse : failedResponse;
     }
 
