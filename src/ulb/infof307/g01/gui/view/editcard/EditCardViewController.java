4e2722df8eab5dae959b562b4c6c4d130e939ce5
louis <louis.vanstappen@gmail.com>
2023-03-29 22:47:39 +0200

    Working demo version of HTML preview

--- src/ulb/infof307/g01/gui/view/editcard/EditCardViewController.java
+++ src/ulb/infof307/g01/gui/view/editcard/EditCardViewController.java
@@ -4,11 +4,13 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.web.HTMLEditor;
+import javafx.scene.web.WebView;
 
 public class EditCardViewController {
     @FXML
     public HTMLEditor htmlEditor;
     public Button saveButton;
+    public WebView webView;
 
     private Listener listener;
 
@@ -20,6 +22,7 @@ public class EditCardViewController {
     @FXML
     public void onSaveButtonClicked(MouseEvent mouseEvent) {
         listener.saveButtonClicked(htmlEditor.getHtmlText());
+        webView.getEngine().loadContent(htmlEditor.getHtmlText());
     }
 
     public interface Listener {
