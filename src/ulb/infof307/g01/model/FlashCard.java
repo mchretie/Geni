bff43247fd8e1d37e6e2d69e2f4e2f30a35564a9
louis <louis.vanstappen@gmail.com>
2023-04-08 15:02:15 +0200

    Basic FlashCard implementation

---
+++ src/ulb/infof307/g01/model/FlashCard.java
@@ -0,0 +1,56 @@
+package ulb.infof307.g01.model;
+
+import com.google.gson.annotations.Expose;
+
+import java.util.Objects;
+import java.util.UUID;
+
+
+public class FlashCard extends Card {
+    @Expose
+    private String back;
+
+    public FlashCard(String front, String back) {
+        super(front);
+        this.back = back;
+    }
+
+    public FlashCard(UUID uuid, UUID deckId, String front, String back) {
+        super(uuid, deckId, front);
+        this.back = back;
+    }
+
+
+    public FlashCard(String front, String back, KnowledgeLevel knowledge) {
+        super(front, knowledge);
+        this.back = back;
+    }
+
+    public String getBack() { return back; }
+
+    public void setBack(String back) { this.back = back; }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.getId(), this.getDeckId(), front, back);
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        UUID id = this.getId();
+        UUID deckId = this.getDeckId();
+        String front = this.getFront();
+
+        if (o == this)
+            return true;
+        if (o == null || o.getClass() != this.getClass())
+            return false;
+
+        FlashCard other = (FlashCard) o;
+        return id.equals(other.getId())
+                && (deckId == other.getDeckId() || deckId.equals(other.getDeckId()))
+                && front.equals(other.getFront());
+    }
+
+}
