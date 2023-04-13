e0fa84a5e9efe03298c410dafb0058ccc0ab0f86
Thomas Maréchal <thomas.marechal@ulb.be>
2023-04-14 00:18:51 +0200

    PlayerScoreItem componenent used in the leaderboard list

---
+++ src/ulb/infof307/g01/gui/view/leaderboard/PlayerScoreItemViewController.java
@@ -0,0 +1,31 @@
+package ulb.infof307.g01.gui.view.leaderboard;
+
+public class PlayerScoreItemViewController {
+    /* ====================================================================== */
+    /*                                Listener                                */
+    /* ====================================================================== */
+
+    private Listener listener;
+
+
+    /* ====================================================================== */
+    /*                                Setter                                  */
+    /* ====================================================================== */
+
+    public void setListener(Listener listener) {
+        this.listener = listener;
+    }
+
+    public void setPlayerScoreItem() {
+        //TODO
+    }
+
+
+    /* ====================================================================== */
+    /*                              Listener                                  */
+    /* ====================================================================== */
+
+    public interface Listener {
+
+    }
+}
