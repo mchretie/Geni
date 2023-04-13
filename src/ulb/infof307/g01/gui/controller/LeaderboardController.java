6f31bfbfd560e772c27dd4c8affddb178eed2b83
Thomas Maréchal <thomas.marechal@ulb.be>
2023-04-14 00:23:00 +0200

    - Adding the controller method to leaderboard files to be able to display a list of playerScoreItem component. - IMPORTANT : need the backend to have the score of the players - TODO : implement setBoard in LeaderboardViewController and loadBoard in LeaderboardController

--- src/ulb/infof307/g01/gui/controller/LeaderboardController.java
+++ src/ulb/infof307/g01/gui/controller/LeaderboardController.java
@@ -1,13 +1,19 @@
 package ulb.infof307.g01.gui.controller;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.stage.Stage;
 import ulb.infof307.g01.gui.util.ImageLoader;
 import ulb.infof307.g01.gui.view.leaderboard.LeaderboardViewController;
 import ulb.infof307.g01.gui.view.mainwindow.MainWindowViewController;
+import ulb.infof307.g01.gui.view.leaderboard.PlayerScoreItemViewController;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
-public class LeaderboardController implements LeaderboardViewController.Listener{
+public class LeaderboardController implements LeaderboardViewController.Listener, PlayerScoreItemViewController.Listener{
     private final Stage stage;
     private final ControllerListener controllerListener;
     private final LeaderboardViewController leaderboardViewController;
@@ -40,7 +46,34 @@ public class LeaderboardController implements LeaderboardViewController.Listener
         mainWindowViewController.setLeaderboardViewVisible();
         mainWindowViewController.makeGoBackIconInvisible();
 
+        leaderboardViewController.setBoard(loadBoard());
+
         stage.show();
+    }
+
+    private List<Node> loadBoard() {
+        try {
+            List<Node> PlayersScoreItem = new ArrayList<>();
+            //TODO Display depending of the backend
+            for (int i = 0; i < 10; i++) {
+
+                URL url = PlayerScoreItemViewController.class.getResource("PlayerScoreItemView.fxml");
+                FXMLLoader loader = new FXMLLoader(url);
+
+                Node node = loader.load();
+                PlayerScoreItemViewController playerScoreItemViewController = loader.getController();
+                playerScoreItemViewController.setListener(this);
+                playerScoreItemViewController.setPlayerScoreItem();
+
+                PlayersScoreItem.add(node);
+            }
+
+            return PlayersScoreItem;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     /* ====================================================================== */
--- src/ulb/infof307/g01/gui/view/leaderboard/LeaderboardViewController.java
+++ src/ulb/infof307/g01/gui/view/leaderboard/LeaderboardViewController.java
@@ -1,13 +1,21 @@
 package ulb.infof307.g01.gui.view.leaderboard;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.BorderPane;
-import ulb.infof307.g01.gui.view.deckmenu.DeckMenuViewController;
+
+import java.util.List;
 
 public class LeaderboardViewController {
     public BorderPane borderPane;
     /* ====================================================================== */
     /*                              FXML Attributes                           */
     /* ====================================================================== */
+    @FXML
+    private ListView<Node> boardContainer;
 
 
     /* ====================================================================== */
@@ -22,6 +30,18 @@ public class LeaderboardViewController {
 
     public void setListener(Listener listener) {
         this.listener = listener;
+    }
+
+
+    /* ====================================================================== */
+    /*                              Leaderboard                               */
+    /* ====================================================================== */
+    public void setBoard(List<Node> PlayersScoreItem){
+        //TODO add Players Score item to boardContainer
+        ObservableList<Node> items = FXCollections.observableArrayList(PlayersScoreItem);
+        boardContainer.setItems(items);
+        boardContainer.refresh();
+
     }
 
     /* ====================================================================== */
