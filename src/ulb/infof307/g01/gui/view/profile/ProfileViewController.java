0014116946c689d176a29e1195bee37c111cc575
mchretie <marcus.chretien@ulb.be>
2023-04-14 13:29:04 +0200

    Profile view and view controller

---
+++ src/ulb/infof307/g01/gui/view/profile/ProfileViewController.java
@@ -0,0 +1,45 @@
+package ulb.infof307.g01.gui.view.profile;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class ProfileViewController {
+
+    @FXML
+    private Label usernameLabel;
+
+    /* ====================================================================== */
+    /*                                Listener                                */
+    /* ====================================================================== */
+
+    private Listener listener;
+
+    /* ====================================================================== */
+    /*                                Setters                                 */
+    /* ====================================================================== */
+
+    public void setListener(Listener listener) {
+        this.listener = listener;
+    }
+
+    public void setUsernameDisplay(String username) {
+        usernameLabel.setText(username);
+    }
+
+    /* ====================================================================== */
+    /*                             Click handlers                             */
+    /* ====================================================================== */
+
+    @FXML
+    private void handleLogoutButtonClicked() {
+        listener.logoutButtonClicked();
+    }
+
+    /* ====================================================================== */
+    /*                           Listener Interface                           */
+    /* ====================================================================== */
+
+    public interface Listener {
+        void logoutButtonClicked();
+    }
+}
