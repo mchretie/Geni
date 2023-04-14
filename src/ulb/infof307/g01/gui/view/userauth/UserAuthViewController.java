93da4d8d59f9f1efa6b7de39362ede3a9c3ff855
mchretie <marcus.chretien@ulb.be>
2023-04-14 13:30:59 +0200

    User authentication view and view controller

---
+++ src/ulb/infof307/g01/gui/view/userauth/UserAuthViewController.java
@@ -0,0 +1,160 @@
+package ulb.infof307.g01.gui.view.userauth;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.VBox;
+
+public class UserAuthViewController {
+
+    @FXML
+    private VBox loginVBOX;
+
+    @FXML
+    private TextField loginUsernameField;
+
+    @FXML
+    private PasswordField loginPasswordField;
+
+    @FXML
+    private VBox registerVBOX;
+
+    @FXML
+    private TextField registerUsernameField;
+
+    @FXML
+    private PasswordField registerPasswordField;
+
+    @FXML
+    private PasswordField confirmPasswordField;
+
+    @FXML
+    private Button registerButton;
+
+    /* ====================================================================== */
+    /*                                Listener                                */
+    /* ====================================================================== */
+
+    private Listener listener;
+
+    /* ====================================================================== */
+    /*                                Setters                                 */
+    /* ====================================================================== */
+
+    public void setListener(Listener listener) {
+        this.listener = listener;
+    }
+
+    public void setRegisterPaneVisible() {
+        this.loginVBOX.setVisible(false);
+        this.registerVBOX.setVisible(true);
+    }
+
+    public void setLoginPaneVisible() {
+        this.loginVBOX.setVisible(true);
+        this.registerVBOX.setVisible(false);
+    }
+
+    /*====================================================================== */
+    /*                                Handlers                               */
+    /*====================================================================== */
+
+    private void handleLogin() {
+        String password = this.loginPasswordField.getText();
+        String username = this.loginUsernameField.getText();
+
+        this.loginPasswordField.clear();
+        this.loginUsernameField.clear();
+        listener.loginClicked(username, password);
+    }
+
+    private void handleRegister() {
+        String username =  this.registerUsernameField.getText();
+        String password =  this.registerPasswordField.getText();
+        String confirmPassword = this.confirmPasswordField.getText();
+
+        this.registerUsernameField.clear();
+        this.registerPasswordField.clear();
+        this.confirmPasswordField.clear();
+
+        listener.registerClicked(username, password, confirmPassword);
+    }
+
+    /* ====================================================================== */
+    /*                             Click handlers                             */
+    /* ====================================================================== */
+
+    @FXML
+    private void handleLoginButtonClicked() {
+        handleLogin();
+    }
+
+    @FXML
+    private void handleRegisterButtonClicked() {
+        handleRegister();
+    }
+
+    @FXML
+    private void handleToLoginClicked() {
+        setLoginPaneVisible();
+    }
+
+    @FXML
+    private void handleToRegisterClicked() { setRegisterPaneVisible(); }
+
+    @FXML
+    private void handleRegisterUsernameKeyPressed(KeyEvent keyEvent) {
+        if (keyEvent.getCode().equals(KeyCode.ENTER))
+            registerPasswordField.requestFocus();
+    }
+
+    @FXML
+    private void handleRegisterPasswordKeyPressed(KeyEvent keyEvent) {
+        if (keyEvent.getCode().equals(KeyCode.ENTER))
+            confirmPasswordField.requestFocus();
+    }
+
+    @FXML
+    private void handleRegisterConfPasswordActionEvent(KeyEvent actionEvent) {
+
+        if (!registerPasswordField.getText().equals(confirmPasswordField.getText())) {
+            registerPasswordField.setStyle("-fx-border-color: red");
+            confirmPasswordField.setStyle("-fx-border-color: red");
+            registerButton.setDisable(true);
+        }
+
+        else {
+            registerPasswordField.setStyle("-fx-border-color: green");
+            confirmPasswordField.setStyle("-fx-border-color: green");
+            registerButton.setDisable(false);
+
+            if (actionEvent.getCode().equals(KeyCode.ENTER))
+                handleRegister();
+        }
+    }
+
+    @FXML
+    private void handleLoginUsernameKeyPressed(KeyEvent keyEvent) {
+        if (keyEvent.getCode().equals(KeyCode.ENTER))
+            loginPasswordField.requestFocus();
+    }
+
+    @FXML
+    private void handleLoginPasswordKeyPressed(KeyEvent keyEvent) {
+        if (keyEvent.getCode().equals(KeyCode.ENTER))
+            handleLogin();
+    }
+
+
+    /* ====================================================================== */
+    /*                           Listener Interface                           */
+    /* ====================================================================== */
+
+    public interface Listener {
+        void loginClicked(String username, String password);
+        void registerClicked(String username, String password, String confirmPassword);
+    }
+}
