2e58ae00486de3179515fa9d587765b68e4472dd
louis <louis.vanstappen@gmail.com>
2023-04-16 15:30:48 +0200

    Dark tag color result in white icon and text

--- src/ulb/infof307/g01/gui/view/editdeck/TagViewController.java
+++ src/ulb/infof307/g01/gui/view/editdeck/TagViewController.java
@@ -5,6 +5,8 @@ import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import org.kordamp.ikonli.javafx.FontIcon;
 import ulb.infof307.g01.model.Tag;
 
 public class TagViewController {
@@ -18,6 +20,9 @@ public class TagViewController {
 
     @FXML
     private Label tagNameLabel;
+
+    @FXML
+    public FontIcon trashIcon;
 
 
     /* ====================================================================== */
@@ -46,10 +51,30 @@ public class TagViewController {
         tagPane.setStyle(tagPane.getStyle() + "-fx-background-color: " + color + ";");
     }
 
+    private void determineTagNameLabelColor() {
+        String color = isDarkHex(tag.getColor()) ? "#FFFFFF" : "#000000";
+        tagNameLabel.setStyle(tagNameLabel.getStyle() + "-fx-text-fill: " + color + ";");
+        trashIcon.setIconColor(Color.web(color));
+    }
+
+    private boolean isDarkHex(String hexString) {
+        // Convert the hex string to an RGB color value
+        int r = Integer.parseInt(hexString.substring(1, 3), 16);
+        int g = Integer.parseInt(hexString.substring(3, 5), 16);
+        int b = Integer.parseInt(hexString.substring(5, 7), 16);
+
+        // Calculate the brightness value of the color
+        double brightness = (0.299 * r + 0.587 * g + 0.114 * b) / 255;
+
+        // Return true if the brightness is less than 0.5 (i.e., the color is "dark")
+        return brightness < 0.3;
+    }
+
     public void setTag(Tag tag) {
         this.tag = tag;
         tagNameLabel.setText(tag.getName());
         setTagColor(tag.getColor());
+        determineTagNameLabelColor();
     }
 
 
@@ -75,6 +100,7 @@ public class TagViewController {
 
     public interface Listener {
         void tagNameChanged(Tag tag, String tagName);
+
         void tagDeleted(Tag tag);
     }
 }
