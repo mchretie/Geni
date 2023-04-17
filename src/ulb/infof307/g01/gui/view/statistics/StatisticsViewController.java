d24dcdeff7aa1e891e23209c98d822203b2ddd59
Thomas Maréchal <thomas.marechal@ulb.be>
2023-04-17 16:47:22 +0200

    setter main stat infos in controller view

--- src/ulb/infof307/g01/gui/view/statistics/StatisticsViewController.java
+++ src/ulb/infof307/g01/gui/view/statistics/StatisticsViewController.java
@@ -1,11 +1,21 @@
 package ulb.infof307.g01.gui.view.statistics;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class StatisticsViewController {
 
     /* ====================================================================== */
     /*                              FXML Attributes                           */
     /* ====================================================================== */
-
+    @FXML
+    private Label totalGamesPlayedLabel;
+    @FXML
+    private Label gamesPlayedTodayLabel;
+    @FXML
+    private Label totalDecksLabel;
+    @FXML
+    private Label totalScoreLabel;
 
     /* ====================================================================== */
     /*                                Listener                                */
@@ -25,6 +35,13 @@ public class StatisticsViewController {
     /* ====================================================================== */
     /*                               Statistics                               */
     /* ====================================================================== */
+
+    void setMainStatistics(String totalGamesPlayed, String gamesPlayedToday, String totalDecks, String totalScore) {
+        totalGamesPlayedLabel.setText(totalGamesPlayed);
+        gamesPlayedTodayLabel.setText(gamesPlayedToday);
+        totalDecksLabel.setText(totalDecks);
+        totalScoreLabel.setText(totalScore);
+    }
 
 
     /* ====================================================================== */
