29abb7727ee096b655a5bad3f83fe399d841d7fa
Thomas Maréchal <thomas.marechal@ulb.be>
2023-04-17 12:00:36 +0200

    Creation files

---
+++ src/ulb/infof307/g01/gui/view/statistics/StatisticsViewController.java
@@ -0,0 +1,16 @@
+package ulb.infof307.g01.gui.view.statistics;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class StatisticsViewController extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
---
+++ src/ulb/infof307/g01/model/Statistics.java
@@ -0,0 +1,4 @@
+package ulb.infof307.g01.model;
+
+public class Statistics {
+}
