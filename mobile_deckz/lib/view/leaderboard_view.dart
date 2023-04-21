af8246830053f693f0c1ab2096fdb67f66c89d7e
louis <louis.vanstappen@gmail.com>
2023-04-21 23:03:04 +0200

    Implemented leaderboard view

--- mobile_deckz/lib/view/leaderboard_view.dart
+++ mobile_deckz/lib/view/leaderboard_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:mobile_deckz/http_dao/leaderboard_dao.dart';
+
 class LeaderboardView extends StatefulWidget {
   const LeaderboardView({super.key});
 
@@ -10,10 +12,82 @@ class LeaderboardView extends StatefulWidget {
 class _LeaderboardViewState extends State<LeaderboardView> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Leaderboard'),
-      ),
-    );
+    final Future<Leaderboard> leaderboard =
+        LeaderboardDao.getGlobalLeaderboard();
+
+    return FutureBuilder<Leaderboard>(
+        future: leaderboard,
+        builder: (BuildContext context, AsyncSnapshot<Leaderboard> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return const Text('An error occurred while loading the leaderboard',
+                style: TextStyle(color: Colors.red));
+          } else {
+            List<Score> scores = snapshot.data?.getLeaderboard() ?? [];
+            Score userScore = snapshot.data?.getUserScore() ??
+                Score(username: '', score: 'N/A', rank: 'N/A');
+            return Scaffold(
+              body: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Your Rank:',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          userScore.rank,
+                          style: const TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        const Text(
+                          'Your Score:',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          userScore.score,
+                          style: const TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const Divider(),
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: scores.length,
+                      itemBuilder: (context, index) {
+                        final score = scores[index];
+                        return ListTile(
+                            leading: Text(score.rank),
+                            title: Text(score.username),
+                            trailing:
+                                Row(mainAxisSize: MainAxisSize.min, children: [
+                              const Icon(Icons.emoji_events),
+                              Text(score.score),
+                            ]));
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            );
+          }
+        });
   }
 }
