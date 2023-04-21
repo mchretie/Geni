62e36ab85e7226b1db6213232e93a6b6169a37cc
louis <louis.vanstappen@gmail.com>
2023-04-21 18:52:26 +0200

    Completed server path

--- mobile_deckz/lib/http_dao/server_path.dart
+++ mobile_deckz/lib/http_dao/server_path.dart
@@ -1,6 +1,6 @@
 class ServerPath {
-  static final Uri baseUrl = Uri.parse("http://localhost:8080");
-  static const String base = "/api";
+  static const String baseUrl = "http://10.0.2.2:8080";
+  static const String base = "$baseUrl/api";
 
   // Used by UserRequestHandler
   static final Uri userBasePath = Uri.parse("$base/user");
