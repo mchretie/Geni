d1377b7f04f59bcc93e3573e3d920bd31c3ee9c0
louis <louis.vanstappen@gmail.com>
2023-04-22 13:23:41 +0200

    Added localhost ip for ios sim

--- mobile_deckz/lib/http_dao/server_path.dart
+++ mobile_deckz/lib/http_dao/server_path.dart
@@ -1,5 +1,6 @@
 class ServerPath {
-  static const String baseUrl = "http://10.0.2.2:8080";
+  // static const String baseUrl = "http://10.0.2.2:8080";
+  static const String baseUrl = "http://192.168.0.191:8080";
   static const String base = "$baseUrl/api";
 
   // Used by UserRequestHandler
