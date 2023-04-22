900f7212b1b5cc7d473662a0563499067848ee14
louis <louis.vanstappen@gmail.com>
2023-04-22 14:11:41 +0200

    Added getDeck method

--- mobile_deckz/lib/http_dao/deck_dao.dart
+++ mobile_deckz/lib/http_dao/deck_dao.dart
@@ -4,33 +4,7 @@ import 'package:http/http.dart' as http;
 import 'package:mobile_deckz/http_dao/http_dao.dart';
 import 'package:mobile_deckz/http_dao/server_path.dart';
 
-class Deck {
-  final String id;
-  final String name;
-  final String color;
-  final String image;
-  final String cardCount;
-  final List<String> tags;
-
-  Deck(
-      {required this.id,
-      required this.name,
-      required this.color,
-      required this.image,
-      required this.cardCount,
-      required this.tags});
-
-  factory Deck.fromJson(Map<String, dynamic> json) {
-    return Deck(
-      id: json['id'],
-      name: json['name'],
-      color: json['color'],
-      image: json['image'],
-      cardCount: json['cardCount'].toString(),
-      tags: json['tags'].cast<String>(),
-    );
-  }
-}
+import '../model/deck/deck.dart';
 
 class DeckDao {
   static Future<List<Deck>> getAllDecks() async {
@@ -40,10 +14,39 @@ class DeckDao {
       List<dynamic> json = jsonDecode(response.body);
       List<Deck> decks = [];
       for (var deck in json) {
+        print(deck);
         decks.add(Deck.fromJson(deck));
       }
       return decks;
     }
     return [];
   }
+
+  static Future<void> getDeck(String id) async {
+    Map<String, String> queryParameters = {
+      'deck_id': id,
+    };
+    final uri = Uri.parse(ServerPath.getDeckPath)
+        .replace(queryParameters: queryParameters);
+    print(uri);
+    final http.Response response = await HttpDao.get(uri);
+    if (response.statusCode == 200) {
+      Map<String, dynamic> json = jsonDecode(response.body);
+      print(json);
+    }
+  }
+
+// static Future<List<Deck>> searchDecks(String query) async {
+//   final http.Response response =
+//       await HttpDao.get(ServerPath.searchDecksPath + query);
+//   if (response.statusCode == 200) {
+//     List<dynamic> json = jsonDecode(response.body);
+//     List<Deck> decks = [];
+//     for (var deck in json) {
+//       decks.add(Deck.fromJson(deck));
+//     }
+//     return decks;
+//   }
+//   return [];
+// }
 }
