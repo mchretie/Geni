58e80749b152a1f2b3acc5bb4acbc8f600cc0ced
Zoé Moeremans <zoe.moeremans@ulb.be>
2023-04-23 18:37:43 +0200

    game history dao

---
+++ mobile_deckz/lib/http_dao/game_history_dao.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:mobile_deckz/http_dao/server_path.dart';
+
+import '../model/game_history/game.dart';
+import '../model/game_history/game_history.dart';
+
+class GameHistoryDao {
+  static Future<GameHistory> getGameHistory() async {
+    http.Response response = await http.get(ServerPath.gameHistoryPath);
+    if (response.statusCode == 200) {
+      List<dynamic> json = jsonDecode(response.body);
+      List<Game> games = [];
+      for (var game in json) {
+        games.add(Game.fromJson(game));
+      }
+      return GameHistory(games: games);
+    }
+    return GameHistory(games: []);
+  }
+}
