2a89a8d6eddb91273dd8cdb1a340b9588ed62822
Thomas <thomas.ocuilleanain@ulb.be>
2023-04-25 14:50:12 +0200

    Added before each

--- test/ulb/infof307/g01/model/leaderboard/GlobalLeaderboardTest.java
+++ test/ulb/infof307/g01/model/leaderboard/GlobalLeaderboardTest.java
@@ -1,5 +1,6 @@
 package ulb.infof307.g01.model.leaderboard;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -9,8 +10,10 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class GlobalLeaderboardTest {
 
-    @Test
-    void getUserScore() {
+    GlobalLeaderboard leaderboard;
+
+    @BeforeEach
+    void setUp() {
         GlobalLeaderboardEntry entry
                 = new GlobalLeaderboardEntry(100, "cutie");
 
@@ -25,8 +28,11 @@ class GlobalLeaderboardTest {
         entries.add(entry2);
         entries.add(entry3);
 
-        GlobalLeaderboard leaderboard = new GlobalLeaderboard(entries);
+        leaderboard = new GlobalLeaderboard(entries);
+    }
 
+    @Test
+    void getUserScore() {
         assertEquals("100", leaderboard.getUserScore("cutie"));
         assertEquals("200", leaderboard.getUserScore("cutie2"));
         assertEquals("300", leaderboard.getUserScore("cutie3"));
@@ -34,22 +40,6 @@ class GlobalLeaderboardTest {
 
     @Test
     void getUserRank() {
-        GlobalLeaderboardEntry entry
-                = new GlobalLeaderboardEntry(100, "cutie");
-
-        GlobalLeaderboardEntry entry2
-                = new GlobalLeaderboardEntry(200, "cutie2");
-
-        GlobalLeaderboardEntry entry3
-                = new GlobalLeaderboardEntry(300, "cutie3");
-
-        List<GlobalLeaderboardEntry> entries = new ArrayList<>();
-        entries.add(entry);
-        entries.add(entry2);
-        entries.add(entry3);
-
-        GlobalLeaderboard leaderboard = new GlobalLeaderboard(entries);
-
         assertEquals("3", leaderboard.getUserRank("cutie"));
         assertEquals("2", leaderboard.getUserRank("cutie2"));
         assertEquals("1", leaderboard.getUserRank("cutie3"));
@@ -57,22 +47,6 @@ class GlobalLeaderboardTest {
 
     @Test
     void forEach() {
-        GlobalLeaderboardEntry entry
-                = new GlobalLeaderboardEntry(100, "cutie");
-
-        GlobalLeaderboardEntry entry2
-                = new GlobalLeaderboardEntry(200, "cutie2");
-
-        GlobalLeaderboardEntry entry3
-                = new GlobalLeaderboardEntry(300, "cutie3");
-
-        List<GlobalLeaderboardEntry> entries = new ArrayList<>();
-        entries.add(entry);
-        entries.add(entry2);
-        entries.add(entry3);
-
-        GlobalLeaderboard leaderboard = new GlobalLeaderboard(entries);
-
         int i = 1;
         for (GlobalLeaderboardEntry e : leaderboard) {
             assertEquals(i + "", e.getRank());
