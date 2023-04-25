e366fbfcd85245c28b4fde0d74e0995d19a2034f
louis <louis.vanstappen@gmail.com>
2023-04-25 17:25:09 +0200

    Working flash card flip

--- mobile_deckz/lib/view/playdeck/card/flash_card_view.dart
+++ mobile_deckz/lib/view/playdeck/card/flash_card_view.dart
@@ -17,44 +17,80 @@ class FlashcardView extends StatefulWidget {
 
 class _FlashcardViewState extends State<FlashcardView> {
   bool _showFront = true;
+  double y = 0;
+
+  slowFlipY() async {
+    bool previousShowFront = _showFront;
+    while (y < 3.14) {
+      await Future.delayed(const Duration(milliseconds: 10));
+      setState(() {
+        y += 0.10;
+      });
+      if (y > 1.55) {
+        setState(() {
+          _showFront = !previousShowFront;
+        });
+      }
+    }
+    setState(() {
+      y = 0;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedSwitcher(
-      duration: const Duration(seconds: 1),
-      child: Container(
-          key: ValueKey<bool>(_showFront),
-          child: GestureDetector(
-            onTap: () {
-              setState(() {
-                _showFront = !_showFront;
-              });
-            },
-            child: _showFront
-                ? FrontCardView(text: widget.card.front)
-                : FrontCardView(text: widget.card.back),
-          )),
-      transitionBuilder: (child, animation) {
-        return RotationYTransition(
-          animation: animation,
-          child: child,
-        );
-        // final rotateAnim = Tween(begin: pi, end: 0.0).animate(animation);
-        // return AnimatedBuilder(
-        //   animation: rotateAnim,
-        //   child: widget,
-        //   builder: (context, widget) {
-        //     // final isUnder = ValueKey<bool>(_showFront);
-        //     final value = _showFront ? min(rotateAnim.value, pi / 2) : rotateAnim.value;
-        //     return Transform(
-        //       transform: Matrix4.rotationY(value),
-        //       alignment: Alignment.center,
-        //       child: widget,
-        //     );
-        //   },
-        // );
-      },
+    return Center(
+      child: Transform(
+        transform: Matrix4.identity()
+          ..setEntry(3, 2, 0.003)
+          ..rotateY(y),
+        alignment: FractionalOffset.center,
+        child: GestureDetector(
+          onTap: () {
+            slowFlipY();
+          },
+          child: _showFront
+              ? FrontCardView(text: widget.card.front)
+              : FrontCardView(text: widget.card.back),
+        ),
+      ),
     );
+
+    //   AnimatedSwitcher(
+    //   duration: const Duration(seconds: 1),
+    //   child: Container(
+    //       key: ValueKey<bool>(_showFront),
+    //       child: GestureDetector(
+    //         onTap: () {
+    //           setState(() {
+    //             _showFront = !_showFront;
+    //           });
+    //         },
+    //         child: _showFront
+    //             ? FrontCardView(text: widget.card.front)
+    //             : FrontCardView(text: widget.card.back),
+    //       )),
+    //   transitionBuilder: (child, animation) {
+    //     return RotationYTransition(
+    //       animation: animation,
+    //       child: child,
+    //     );
+    //     // final rotateAnim = Tween(begin: pi, end: 0.0).animate(animation);
+    //     // return AnimatedBuilder(
+    //     //   animation: rotateAnim,
+    //     //   child: widget,
+    //     //   builder: (context, widget) {
+    //     //     // final isUnder = ValueKey<bool>(_showFront);
+    //     //     final value = _showFront ? min(rotateAnim.value, pi / 2) : rotateAnim.value;
+    //     //     return Transform(
+    //     //       transform: Matrix4.rotationY(value),
+    //     //       alignment: Alignment.center,
+    //     //       child: widget,
+    //     //     );
+    //     //   },
+    //     // );
+    //   },
+    // );
   }
 }
 
