570931e82fd39ebb2d9c49612062577a8101882a
Boris Petrov <boris.petrov@ulb.be>
2023-05-10 13:24:50 +0200

    Correct computation of score + stats at result screen

--- mobile_deckz/lib/model/deck/score.dart
+++ mobile_deckz/lib/model/deck/score.dart
@@ -1,18 +1,61 @@
+import 'dart:math';
+
+class Answer {
+  final bool correctlyAnswered;
+  final num remainingTime;
+  final num totalTime;
+
+  Answer(this.correctlyAnswered, this.remainingTime, this.totalTime);
+}
+
 class Score {
   int _score;
+  final _answers = [];
+
+  final minScore = 0;
+  final maxScore = 1000;
+
   bool _final = false;
 
   Score(this._score);
 
   int get score => _score;
+  double get averageResponseTime => _averageResponseTime();
+  double get totalResponseTime => _totalResponseTime();
 
-  void incrementScore(double remainingTime) {
-    _score += (1000 * remainingTime).round();
+  bool get isFinal => _final;
+
+  void recordAnswer(Answer answer) {
+    _answers.add(answer);
+    _computeScore();
   }
 
   void setFinal() {
     _final = true;
   }
 
-  bool get isFinal => _final;
+  double _averageResponseTime() {
+    return _totalResponseTime() / _answers.length;
+  }
+
+  double _totalResponseTime() {
+    return _answers.fold(
+        0.0, (val, elem) => val + (elem.totalTime - elem.remainingTime));
+  }
+
+  void _computeScore() {
+    var score = _answers.fold(0, (val, elem) => val + _scoreOf(elem));
+    _score = (score / _answers.length).round();
+  }
+
+  int _scoreOf(Answer answer) {
+    var score = 0;
+    if (answer.correctlyAnswered) {
+      final unitRemainingTime = answer.totalTime / answer.remainingTime;
+      final exponent = -8 * unitRemainingTime + 4;
+      final toAdd = maxScore * (1 / (1 + exp(exponent)));
+      score += toAdd.round();
+    }
+    return score;
+  }
 }
--- mobile_deckz/lib/view/playdeck/score_result_view.dart
+++ mobile_deckz/lib/view/playdeck/score_result_view.dart
@@ -14,10 +14,32 @@ class ScoreResultView extends StatelessWidget {
       Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text('Final Score: ', style: TextStyle(fontSize: 20)),
+          const Text('Votre score : ', style: TextStyle(fontSize: 20)),
           Text(score.score.toString(),
               style:
                   const TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+        ],
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('Temps moyen : ', style: TextStyle(fontSize: 20)),
+          Text(score.averageResponseTime.toStringAsFixed(2),
+              style:
+                  const TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+          const Text(' s',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+        ],
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('Temps total : ', style: TextStyle(fontSize: 20)),
+          Text(score.totalResponseTime.toStringAsFixed(2),
+              style:
+                  const TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+          const Text(' s',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
         ],
       ),
       const SizedBox(height: 20),
