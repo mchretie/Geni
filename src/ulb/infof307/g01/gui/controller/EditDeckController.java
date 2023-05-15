25f308765e8fc51bfed808d247869553d06943a4
Thomas <thomas.ocuilleanain@ulb.be>
2023-05-15 13:45:26 +0200

    Readbility increase

--- src/ulb/infof307/g01/gui/controller/EditDeckController.java
+++ src/ulb/infof307/g01/gui/controller/EditDeckController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import ulb.infof307.g01.exception.NeverThrown;
 import ulb.infof307.g01.gui.controller.errorhandler.ErrorHandler;
 import ulb.infof307.g01.gui.http.ServerCommunicator;
 import ulb.infof307.g01.gui.http.exceptions.ServerCommunicationFailedException;
@@ -23,7 +24,7 @@ import java.util.List;
 
 public class EditDeckController implements EditDeckViewController.Listener,
                                                     TagViewController.Listener,
-                                                    CardVisitor<RuntimeException> {
+                                                    CardVisitor<NeverThrown> {
 
     /* ====================================================================== */
     /*                             Model Attributes                           */
