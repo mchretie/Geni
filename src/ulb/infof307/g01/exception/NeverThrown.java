ab03354ab275d3638e5492a2a355c3096d2019ce
Thomas <thomas.ocuilleanain@ulb.be>
2023-05-15 14:12:41 +0200

    Typo

--- src/ulb/infof307/g01/exception/NeverThrown.java
+++ src/ulb/infof307/g01/exception/NeverThrown.java
@@ -2,6 +2,6 @@ package ulb.infof307.g01.exception;
 
 public class NeverThrown extends RuntimeException {
     public NeverThrown() {
-        super("This exception is never be thrown.");
+        super("This exception is never thrown.");
     }
 }
