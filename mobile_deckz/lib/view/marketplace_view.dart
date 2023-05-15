792a889400d6a0f97c71e4e445d7e71297c55c31
mchretie <marcus.chretien@ulb.be>
2023-05-15 16:52:27 +0200

    marketplace pull refresh implemented

--- mobile_deckz/lib/view/marketplace_view.dart
+++ mobile_deckz/lib/view/marketplace_view.dart
@@ -29,67 +29,76 @@ class _MarketPlaceViewState extends State<MarketPlaceView> {
     });
   }
 
+  Future<void> _reloadDecks() async {
+    setState(() {
+      _decksFuture = MarketPlaceDao.getAllMarketplaceDecks();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List<MarketplaceDeck>>(
-        future: _decksFuture,
-        builder: (BuildContext context,
-            AsyncSnapshot<List<MarketplaceDeck>> snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Text('Error: ${snapshot.error}');
-          } else {
-            List<MarketplaceDeck> deckList = snapshot.data ?? [];
-            return Scaffold(
-                body: Center(
-                    child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: <Widget>[
-                  Padding(
-                      padding: const EdgeInsets.all(16),
-                      child: Row(
-                        children: [
-                          DropdownButton<String>(
-                            value: dropdownValue,
-                            onChanged: (String? newValue) {
-                              setState(() {
-                                dropdownValue = newValue!;
-                              });
-                            },
-                            items: dropdownItems
-                                .map<DropdownMenuItem<String>>((String value) {
-                              return DropdownMenuItem<String>(
-                                value: value,
-                                child: Text(value),
-                              );
-                            }).toList(),
-                          ),
-                          const SizedBox(width: 20),
-                          Expanded(
-                            child: TextField(
-                              controller: textEditController,
-                              onSubmitted: (text) => _onSearchTextChanged(text),
-                              decoration: const InputDecoration(
-                                hintText: 'Search',
-                                prefixIcon: Icon(Icons.search),
+    return RefreshIndicator(
+      onRefresh: _reloadDecks,
+      child: FutureBuilder<List<MarketplaceDeck>>(
+          future: _decksFuture,
+          builder: (BuildContext context,
+              AsyncSnapshot<List<MarketplaceDeck>> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            } else {
+              List<MarketplaceDeck> deckList = snapshot.data ?? [];
+              return Scaffold(
+                  body: Center(
+                      child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: <Widget>[
+                    Padding(
+                        padding: const EdgeInsets.all(16),
+                        child: Row(
+                          children: [
+                            DropdownButton<String>(
+                              value: dropdownValue,
+                              onChanged: (String? newValue) {
+                                setState(() {
+                                  dropdownValue = newValue!;
+                                });
+                              },
+                              items: dropdownItems
+                                  .map<DropdownMenuItem<String>>((String value) {
+                                return DropdownMenuItem<String>(
+                                  value: value,
+                                  child: Text(value),
+                                );
+                              }).toList(),
+                            ),
+                            const SizedBox(width: 20),
+                            Expanded(
+                              child: TextField(
+                                controller: textEditController,
+                                onSubmitted: (text) => _onSearchTextChanged(text),
+                                decoration: const InputDecoration(
+                                  hintText: 'Search',
+                                  prefixIcon: Icon(Icons.search),
+                                ),
                               ),
                             ),
-                          ),
-                        ],
-                      )),
-                  Expanded(
-                      child: ListView.builder(
-                          itemCount: deckList.length,
-                          itemBuilder: (context, index) {
-                            final MarketplaceDeck deck = deckList[index];
-                            return Padding(
-                                padding: const EdgeInsets.all(4.0),
-                                child: DeckView(deck: deck));
-                          }))
-                ])));
-          }
-        });
+                          ],
+                        )),
+                    Expanded(
+                        child: ListView.builder(
+                            itemCount: deckList.length,
+                            itemBuilder: (context, index) {
+                              final MarketplaceDeck deck = deckList[index];
+                              return Padding(
+                                  padding: const EdgeInsets.all(4.0),
+                                  child: DeckView(deck: deck));
+                            }))
+                  ])));
+            }
+          }),
+    );
   }
 }
